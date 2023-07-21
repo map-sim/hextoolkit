8dcc2c12f6fab893ed0f09fea67c1b360d0e3459
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2023-07-21 02:16:12 +0200

    +

--- MapExamples.py
+++ MapExamples.py
@@ -2,33 +2,59 @@ library0 = {
     "objects": {
 	"drill": {"shape": "drill-0", "radius": 30},
 	"mineshaft": {"shape": "mineshaft-0", "radius": 30},
-	"input": {},
-	"output": {},
-	"store": {},
-	"mixer": {},
-	"repeater": {},
-	"developer": {},
-	"transmitter": {},
-	"laboratory": {},
-	"launcher": {},
-	"radiator": {},
-	"barrier": {}
+	"input": {"shape": "input-0", "radius": 30},
+	"output": {"shape": "output-0", "radius": 30},
+	"store": {"shape": "store-0", "radius": 30},
+	"mixer": {"shape": "mixer-0", "radius": 30},
+	"repeater": {"shape": "repeater-0", "radius": 30},
+	"developer": {"shape": "developer-0", "radius": 30, "fuel": "AC"},
+	"launcher": {"shape": "launcher-0", "radius": 30, "fuel": "AC"},
+	"transmitter": {"shape": "transmitter-0", "radius": 60, "fuel": "AB"},
+	"laboratory": {"shape": "laboratory-0", "radius": 30, "fuel": "AB"},
+	"radiator": {"shape": "radiator-0", "radius": 60, "fuel": "BC"},
+	"barrier": {"shape": "barrier-0", "radius": 30, "fuel": "BC"}
+    },
+    "resources": {
+        "A": {"color": [1.0, 0.1, 0.1]},
+        "B": {"color": [0.1, 0.8, 0.1]},
+        "C": {"color": [0.1, 0.1, 1.0]},
+        "AB": {"color": [1.0, 0.8, 0.1]},
+        "AC": {"color": [1.0, 0.1, 1.0]},
+        "BC": {"color": [0.1, 0.9, 0.9]}
     },
     "terrains": {
-	"desert-0": {"desc": "desert", "color": [1.0, 1.0, 1.0],    "level": 10.5},
-	"desert-1": {"desc": "desert", "color": [0.92, 0.92, 0.92], "level": 14.2},
-	"desert-2": {"desc": "desert", "color": [0.84, 0.84, 0.84], "level": 32.3}
+	"desert-0": {"desc": "desert", "color": [1.0, 0.98, 0.95], "level": 10.5},
+	"desert-1": {"desc": "desert", "color": [1.0, 0.92, 0.92], "level": 14.2},
+	"desert-2": {"desc": "desert", "color": [1.0, 0.84, 0.84], "level": 32.3}
     },
     "players": {
-        "Aaa": {"color": [1.0, 1.0, 0.0]}
+        "Aaa": {"color": [1.0, 1.0, 0.5]},
+        "Bbb": {"color": [1.0, 0.5, 1.0]}
     }
 }
 
 battlefield0 = {
     "objects": [
-        ("mineshaft", -380, -120, "Aaa", 1.0),
+        ("mineshaft", -380, -120, "Aaa", 1.0, "A"),
+        ("mineshaft", -280, -120, "Bbb", 1.0, "B"),
+        ("mineshaft", -180, -120, "Bbb", 1.0, "C"),
         ("drill", -340, -200, "Aaa", 1.0),
-        ("drill", -300, -250, "Aaa", 0.66)
+        ("drill", 140, -200, "Bbb", 1.0),
+        ("drill", -300, -250, "Aaa", 0.1),
+        ("input", -140, -200, "Aaa", 1.0, "A"),
+        ("output", -100, -250, "Aaa", 0.66, "A"),
+        ("store", -300, 50, "Aaa", 0.5, "A", 166.0),
+        ("mixer", -200, -250, "Aaa", 0.5, None),
+        ("mixer", -200, -450, "Aaa", 0.5, "BC"),
+        ("mixer", -100, -450, "Aaa", 1.0, "AB"),
+        ("mixer", 0, -450, "Aaa", 0.5, "AC"),
+        ("launcher", 200, 250, "Aaa", 1.0, (-300, -250)),
+        ("radiator", 100, 250, "Aaa", 1.0, True),
+        ("laboratory", 100, 150, "Aaa", 1.0, "tech"),
+        ("barrier", 0, 250, "Aaa", 1.0, True),
+        ("developer", 200, 150, "Aaa", 1.0, True),
+        ("transmitter", 300, 150, "Aaa", 1.0, True),
+        ("repeater", 400, 150, "Aaa", 1.0)
     ],
     "resorces": [
     ],
