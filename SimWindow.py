7455bbbbd78057542cc0b0564becae13a1802dcc
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2023-08-19 03:43:32 +0200

    +

--- SimWindow.py
+++ SimWindow.py
@@ -12,121 +12,130 @@ from gi.repository import Gdk
 
 import math
 TWO_PI = 2 * math.pi
+
+class SimObject:
+    black_color = 0, 0, 0, 1
+
+    def __init__(self, config, library, context, xyloc, player, modules):
+        self.config, self.library = config, library
+        self.color = self.library["players"][player]["color"]
+        self.xy = self.__calc_render_xy(*xyloc)
+        self.context = context
+        self.modules = modules
+        self.xyloc = xyloc
+
+    def __calc_render_xy(self, xloc, yloc):
+        zoom = self.config["window-zoom"]
+        xloc, yloc = xloc * zoom, yloc * zoom
+        xoffset, yoffset = self.config["window-offset"]
+        return xloc + xoffset, yloc + yoffset
+
+    def _draw_line(self, xy0, xy1, color, width=None):
+        if width is not None: self.context.set_line_width(width)
+        self.context.set_source_rgba(*color)
+        xy0  = self.xy[0] + xy0[0], self.xy[1] + xy0[1]
+        xy1  = self.xy[0] + xy1[0], self.xy[1] + xy1[1]
+        self.context.move_to(*xy0)
+        self.context.line_to(*xy1) 
+        self.context.stroke()
+    
+    def _draw_polygon(self, points, color, width=None):
+        if width is not None: self.context.set_line_width(width)
+        self.context.set_source_rgba(*color)
+        start_x, start_y = points[-1]
+        self.context.move_to (self.xy[0] + start_x, self.xy[1] + start_y)
+        for point in points:    
+            stop_x, stop_y = point
+            self.context.line_to (self.xy[0] + stop_x, self.xy[1] + stop_y)
+        self.context.fill()
+        self.context.stroke()
+
+    def _draw_resource(self, xy, resource):
+        xy = self.xy[0] + xy[0], self.xy[1] + xy[1]
+        r = 0.34 * self.config["window-zoom"]
+        self.context.set_source_rgba(*self.black_color)
+        self.context.arc(*xy, r, 0, TWO_PI)
+        self.context.fill()
+
+        if resource is None: color2 = self.black_color
+        else: color2 = self.library["resources"][resource]["color"]
+        self.context.set_source_rgba(*color2)
+        rr = 0.18 * self.config["window-zoom"]
+        self.context.arc(*xy, rr, 0, TWO_PI)
+        self.context.fill()
+
+class SimMixer_0(SimObject):
+    def __init__(self, config, library, context, xyloc, player, modules, resource):
+        SimObject.__init__(self, config, library, context, xyloc, player, modules)
+        self.resource = resource
+
+    def draw(self):
+        r = 1 * self.config["window-zoom"]
+        self.context.set_source_rgba(*self.black_color)
+        self.context.arc(*self.xy, r, 0, TWO_PI)
+        self.context.fill()
+        self.context.set_source_rgba(*self.color)
+        self.context.arc(*self.xy, 3*r/4, 0, TWO_PI)
+        self.context.fill()
+
+        w = 0.15 * self.config["window-zoom"]
+        self.context.set_line_width(w)
         
+        rr = r * 2/3 
+        self.context.set_source_rgba(*self.black_color)
+        xi0, yi0 = self.xy[0] - rr, self.xy[1] - rr 
+        xe0, ye0 = self.xy[0] + rr, self.xy[1] + rr
+        self.context.move_to(xi0, yi0)
+        self.context.line_to(xe0, ye0) 
+        self.context.stroke()
+        xi1, yi1 = self.xy[0] - rr, self.xy[1] + rr 
+        xe1, ye1 = self.xy[0] + rr, self.xy[1] - rr
+        self.context.move_to(xi1, yi1)
+        self.context.line_to(xe1, ye1) 
+        self.context.stroke()
+
+        self._draw_resource((0, 0), self.resource)
+
+class SimStore_0(SimObject):
+    cells = [(-0.43, -0.84), (0.43, -0.84), (-0.43, 0),
+             (0.43, 0), (-0.43, 0.84), (0.43, 0.84)]    
+    def __init__(self, config, library, context, xyloc, player, modules, resources):
+        SimObject.__init__(self, config, library, context, xyloc, player, modules)
+        self.resources = resources
+
+    def draw(self):
+        r = 0.82 * self.config["window-zoom"]
+        rr = 0.66 * self.config["window-zoom"]
+        w = 0.1 * self.config["window-zoom"]
+     
+        self.context.set_line_width(w)
+        self.context.set_source_rgba(*self.black_color)
+        pts0 = [(-r, -1.5*r), (-r, +1.5*r), (+r, +1.5*r), (+r, -1.5*r)]
+        pts1 = [(-rr, -1.5*rr), (-rr, +1.5*rr), (+rr, +1.5*rr), (+rr, -1.5*rr)]
+        self._draw_polygon(pts0, self.black_color)
+        self._draw_polygon(pts1, self.color)
+
+        self._draw_line((-r, -0.42*r), (+r, -0.42*r), self.black_color)
+        self._draw_line((-r, +0.42*r), (+r, +0.42*r), self.black_color)
+        self._draw_line((0, -1.45*r), (0, +1.45*r), self.black_color)
+        for i, resource in enumerate(self.resources):
+            xyloc = r * self.cells[i][0], r * self.cells[i][1]
+            self._draw_resource(xyloc, resource)
+
 class SimPainter(TerrPainter):
     def __init__(self, config, library, battlefield):
         self.terr_painter = TerrPainter(config, library, battlefield)
         self.battlefield =  battlefield
         self.library = library
         self.config = config
-
-    def draw_polygon(self, context, color, points):
-        context.set_source_rgba(*color)
-        start_x, start_y = points[-1]
-        context.move_to (start_x, start_y)
-        for point in points:    
-            stop_x, stop_y = point
-            context.line_to (stop_x, stop_y)
-        context.fill()
-        context.stroke()
-
-    def calc_render_params(self, xloc, yloc):
-        xoffset, yoffset = self.config["window-offset"]
-        zoom = self.config["window-zoom"]
-        xloc, yloc = xloc * zoom, yloc * zoom
-        xloc, yloc = xloc + xoffset, yloc + yoffset
-        return xloc, yloc
-
-    def draw_mixer_0(self, context, xyloc, color, modules, params):
-        xloc, yloc = self.calc_render_params(*xyloc)
-        r = 24 * self.config["window-zoom"]
         
-        ob_color = 0, 0, 0, 1
-
-        context.set_source_rgba(*ob_color)
-        context.arc(xloc, yloc, r, 0, TWO_PI)
-        context.fill()
-
-        context.set_source_rgba(*color)
-        context.arc(xloc, yloc, 3*r/4, 0, TWO_PI)
-        context.fill()
-
-        w = 4 * self.config["window-zoom"]
-        context.set_line_width(w)
-        
-        rr = 2*r/3
-        context.set_source_rgba(*ob_color)
-        xi0, yi0 = xloc - rr, yloc - rr 
-        xe0, ye0 = xloc + rr, yloc + rr
-        context.move_to(xi0, yi0)
-        context.line_to(xe0, ye0) 
-        context.stroke()
-        xi0, yi0 = xloc - rr, yloc + rr 
-        xe0, ye0 = xloc + rr, yloc - rr
-        context.move_to(xi0, yi0)
-        context.line_to(xe0, ye0) 
-        context.stroke()
-
-        #xc = xloc + 16 * self.config["window-zoom"]
-        #yc = yloc + 16 * self.config["window-zoom"]
-        # self.draw_control(context, xc, yc, ob_color, resource)
-
-        r = 8 * self.config["window-zoom"]
-        context.set_source_rgba(*ob_color)
-        context.arc(xloc, yloc, r, 0, TWO_PI)
-        context.fill()
-
-        if params[0] is None: color2 = (0.0, 0.0, 0.0)
-        else: color2 = self.library["resources"][params[0]]["color"]
-            
-        r = 5 * self.config["window-zoom"]
-        context.set_source_rgba(*color2)
-        context.arc(xloc, yloc, r, 0, TWO_PI)
-        context.fill()
-
-        
-    def draw_store_0(self, context, xyloc, color, modules, params):
-        xloc, yloc = self.calc_render_params(*xyloc)
-        r = 20 * self.config["window-zoom"]
-        rr = 14 * self.config["window-zoom"]
-
-        w = 4 * self.config["window-zoom"]
-        context.set_line_width(w)
-
-        ob_color = 0, 0, 0, 1
-        context.set_source_rgba(*ob_color)
-        points = [(xloc-r, yloc-r), (xloc-r, yloc+r),
-                  (xloc+r, yloc+r), (xloc+r, yloc-r)]
-        self.draw_polygon(context, ob_color, points)
-        points = [(xloc-rr, yloc-rr), (xloc-rr, yloc+rr),
-                  (xloc+rr, yloc+rr), (xloc+rr, yloc-rr)]
-        self.draw_polygon(context, color, points)
-
-        context.set_source_rgba(*ob_color)
-        xi0, yi0 = xloc - r, yloc 
-        xe0, ye0 = xloc + r, yloc
-        context.move_to(xi0, yi0)
-        context.line_to(xe0, ye0) 
-        context.stroke()
-
-        xi1, yi1 = xloc, yloc - r
-        xe1, ye1 = xloc, yloc + r
-        context.move_to(xi1, yi1)
-        context.line_to(xe1, ye1) 
-        context.stroke()
-
-        #xc = xloc + 16 * self.config["window-zoom"]
-        #yc = yloc + 16 * self.config["window-zoom"]
-        # self.draw_control(context, xc, yc, ob_color, resource)
-
     def draw(self, context):
         self.terr_painter.draw(context)
         for  xyloc, obj, player, modules, *params in self.battlefield["objects"]:
             shape = self.library["objects"][obj]["shape"]
-            color = self.library["players"][player]["color"]
 
-            if shape == "store-0": self.draw_store_0(context, xyloc, color, modules, *params)
-            elif shape == "mixer-0": self.draw_mixer_0(context, xyloc, color, modules, *params)
+            if shape == "mixer-0": SimMixer_0(self.config, self.library, context, xyloc, player, modules, params[0]).draw()
+            elif shape == "store-0": SimStore_0(self.config, self.library, context, xyloc, player, modules, params[0]).draw()
             else: raise ValueError(f"Not supported object: {obj}")
 
 class SimWindow(TerrWindow):
@@ -165,6 +174,8 @@ class SimWindow(TerrWindow):
             self.mode = "admin"
             self.draw_content()
 
+        elif key_name == "i" and self.mode == "navi":
+            print(self.config)
         elif key_name == "i" and self.mode == "admin":
             for key, val in self.config.items():
                 print(f"config {key}", "-->", val)
@@ -195,8 +206,8 @@ def run_example():
     example_config = {
         "window-title": "mode-window",
         "window-size": (1800, 820),
-        "window-offset": (750, 400),
-        "window-zoom": 0.366,
+        "window-offset": (840, 125),
+        "window-zoom": 36.0,
         "move-sensitive": 50
     }
     
@@ -225,4 +236,5 @@ def run_example():
 
 # broadwayd :5
 # GDK_BACKEND=broadway BROADWAY_DISPLAY=:5 python3 ...
+# http://127.0.0.1:8085/
 if __name__ == "__main__": run_example()
