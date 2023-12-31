939ea98898685fbfac71f251fc690fb49a30607d
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2023-12-31 14:18:23 +0100

    +

---
+++ engine-0/engine.py
@@ -0,0 +1,38 @@
+from diagram import Diagram
+
+
+class Engine:
+    constants = {
+        "<science>-factor": 1.0,
+        "<industrial>-factor": 1.0,
+        "<consumption>-factor": 1.0,
+        "<development>-factor": 1.0,
+        "base-build-capacity": 10.0,
+        "base-node-capacity": 1000.0
+    }
+    technologies = [
+        ("production", "A"),
+        ("production", "B"),
+        ("production", "C"),
+        ("mixing", "ABC"),
+        ("mixing", "AB"),
+        ("mixing", "BC"),
+        ("mixing", "AC")
+    ]
+    processes = {
+        "A": None,
+        "B": None,
+        "C": None,
+        "AB": {"A": 1.0, "B": 1.0},
+        "BC": {"B": 1.0, "C": 1.0},
+        "AC": {"A": 1.0, "C": 1.0},
+        "ABC": {"A": 1.0, "B": 1.0, "C": 1.0},
+        "<consumption>": "AB",
+        "<development>": "BC",
+        "<industrial>": "AC",
+        "<science>": "ABC"
+    }
+
+    def __init__(self):
+        self.controls = []
+        self.diagram = Diagram()
