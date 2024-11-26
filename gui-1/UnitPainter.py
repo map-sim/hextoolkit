cf3306da5e910612cf806340a16b01bb4d85d146
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2024-11-26 23:47:17 +0100

    +

--- gui-1/UnitPainter.py
+++ gui-1/UnitPainter.py
@@ -5,28 +5,28 @@ class UnitPainter(AbstractPainter):
     def draw(self, context):
         for vex, units in self.saver.units.items():
             own, symbol, size = self.estimate_unit(units)
-            if symbol == "infantry": self.draw_unit_infantry(context, vex, own, size)
-            else: raise ValueError(f"Not supported unit type: {unit['type']}")
-
+            self.draw_unit_envelop(context, vex, own)
+            self.draw_unit_label(context, vex, symbol, size)
+            
     def estimate_unit(self, units):
         size = 0
         for unit in units:
             size += unit["size"]
-        return units[0]["own"], units[0]["type"], size
+        return units[0]["own"], "K", size
 
-    def draw_unit_size(self, context, vex, size):
+    def draw_unit_label(self, context, vex, symbol, size):
         r = self.saver.settings.get("hex-radius", 1.0)
         zoom = self.saver.settings["window-zoom"]
         loc = AbstractPainter.vex_to_loc(vex, r)                
         xo, yo = self.translate_xy(*loc)
-        if size < 10: context.move_to(xo-0.3*zoom, yo+0.35*zoom)
-        else: context.move_to(xo-0.6*zoom, yo+0.35*zoom)
-        context.set_font_size(0.94*zoom)
+        if size < 10: context.move_to(xo-0.5*zoom, yo+0.35*zoom)
+        else: context.move_to(xo-0.75*zoom, yo+0.35*zoom)
+        context.set_font_size(0.78*zoom)
         context.set_source_rgba(0, 0, 0)
-        context.show_text(str(size))
+        context.show_text(f"{symbol}{size}")
         context.fill()
 
-    def draw_unit_infantry(self, context, vex, own, size):
+    def draw_unit_envelop(self, context, vex, own):
         r = self.saver.settings.get("hex-radius", 1.0)
         zoom = self.saver.settings["window-zoom"]
         color = self.saver.controls[own]["unit-color"]
@@ -56,5 +56,3 @@ class UnitPainter(AbstractPainter):
         context.line_to(xo - 0.74 * zoom, yo - 0.54 * zoom)
         context.line_to(xo - 0.74 * zoom, yo + 0.54 * zoom)
         context.fill()
-
-        self.draw_unit_size(context, vex, size)
